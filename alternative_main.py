1a22c764c5c5ca0543c316abc617379abe95bae2
Biagio Licari <biagiolicari@outlook.com>
2024-05-20 16:40:17 +0200

    added gpt4vision

--- alternative_main.py
+++ alternative_main.py
@@ -4,10 +4,10 @@ import cv2
 from dotenv import load_dotenv
 
 from azure_detect.azurefacedetect import FaceDetectionRecognitionAzure
+from azure_ocr.ocr import OCR
 from azure_speech.synthesizer import Synthesizer
 from azure_speech.voice import Voice
 from azure_translate.translate import Translate
-from azure_ocr.ocr import OCR
 from cnn_detection.facenet_detection import FaceDetectionRecognition
 from large_language_model.bot_gpt import BotAgent
 
@@ -23,7 +23,7 @@ def load_env_variables(required_vars):
     return env_vars
 
 
-def setup_camera(width=320, height=240):
+def setup_camera(width=128, height=128):
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
@@ -33,7 +33,7 @@ def setup_camera(width=320, height=240):
 
 
 def initialize_services(env_vars):
-    bot = BotAgent(env_vars["key_openai"], env_vars["endpoint_openai"], env_vars["model_openai"])
+    bot = BotAgent(env_vars["key_openai"], env_vars["endpoint_openai"], env_vars["model_openai"], env_vars['model_openai_vision'])
     voice = Voice(env_vars["region_multiservice"], env_vars["key_multiservice"])
     synthesizer = Synthesizer(env_vars["region_multiservice"], env_vars["key_multiservice"])
     translate = Translate(env_vars["region_multiservice"], env_vars["key_multiservice"],
@@ -54,8 +54,10 @@ def detect_faces_facenet(frame, facenet):
     boxes, confidences, nfaces = facenet.detect_faces(img_rgb)
     return boxes, confidences, nfaces, img_rgb
 
+
 def text_from_image(ocr, img_rgb, detected_language):
     return ocr.ImageOCR(img_rgb, detected_language)
+
 
 def count_faces(cap, batch_size=30):
     facenet = FaceDetectionRecognition()
@@ -66,6 +68,9 @@ def count_faces(cap, batch_size=30):
         ret, frame = cap.read()
         if not ret:
             raise Exception("Error: Failed to capture image.")
+
+        # Resize the frame early to speed up processing
+        frame = cv2.resize(frame, (64, 64))
 
         # FaceNet detection
         boxes, confidences, nfaces, img_rgb = detect_faces_facenet(frame, facenet)
@@ -92,7 +97,59 @@ def count_faces(cap, batch_size=30):
     return best_frame[0], best_frame[1]
 
 
-def interact_with_user(bot, voice, synthesizer, translate, nfaces):
+def get_frames_from_camera(cap, batch_size=50, output_filename="best_frame.jpg"):
+    """
+    Capture n frames return the clearest frame based on Laplacian variance.
+
+    :param cap: cv2.VideoCapture object
+    :param batch_size: Number of frames to capture
+    :return: best frame
+    """
+    frame_buffer = []
+    max_frames = batch_size
+
+    for _ in range(max_frames):
+        ret, frame = cap.read()
+        if not ret:
+            raise Exception("Error: Failed to capture image.")
+
+        cv2.imshow('Webcam Face Detection', frame)
+        frame_buffer.append(frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    if not frame_buffer:
+        return None
+
+    def calculate_blur_score(frame):
+        """
+        Calculate the Laplacian variance of the frame to estimate its clarity.
+        Higher variance indicates a clearer frame.
+
+        :param frame: Image frame
+        :return: Variance value
+        """
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        variance = cv2.Laplacian(gray, cv2.CV_64F).var()
+        return variance
+
+    # Evaluate clarity of each frame
+    frame_scores = [(frame, calculate_blur_score(frame)) for frame in frame_buffer]
+
+    # Select the frame with the highest clarity score
+    best_frame = max(frame_scores, key=lambda x: x[1])[0]
+
+    # Resize the best frame to 64x64 pixels
+    resized_best_frame = cv2.resize(best_frame, (48, 48), interpolation=cv2.INTER_AREA)
+
+    # Save the resized best frame as a JPEG file
+    cv2.imwrite(output_filename, resized_best_frame)
+
+    return output_filename
+
+
+def interact_with_user(bot, voice, synthesizer, translate, nfaces, cap):
     if nfaces == 1:
         synthesizer.synthesizer("Ciao! Ho visto che sei solo, come posso aiutarti?", "it-IT")
     else:
@@ -106,11 +163,13 @@ def interact_with_user(bot, voice, synthesizer, translate, nfaces):
             elif text.lower() == translate.translate("esci.", detected_lang[:2]):
                 synthesizer.synthesizer(translate.translate("A presto!", detected_lang[:2]), detected_lang)
                 return False  # Exit application
-            elif text.lower() == translate.translate("Per favore descrivimi questa immagine.", detected_lang[:2]):
-                img_jpg = open("image.jpg", "rb").read()
-                response = bot.food_analyzer("Descrivi questa immagine.", img_jpg,)
-                return False
-                
+            elif text.lower() == translate.translate("esprimi un tuo parere su ciò che vedi dalla webcam.",
+                                                     detected_lang[:2]):
+                frame_from_webcam = get_frames_from_camera(cap)
+                res = bot.chat_with_vision(frame_from_webcam)
+                synthesizer.synthesizer(response_text=res, speech_recognition_language=detected_lang)
+                continue
+
             else:
                 response = bot.chat(text)
                 synthesizer.synthesizer(response, detected_lang)
@@ -124,7 +183,7 @@ def interact_with_user(bot, voice, synthesizer, translate, nfaces):
 
 def main():
     required_vars = ["key_multiservice", "endpoint_multiservice", "region_multiservice",
-                     "key_openai", "endpoint_openai", "model_openai", "prompt_system"]
+                     "key_openai", "endpoint_openai", "model_openai", "prompt_system", "model_openai_vision"]
 
     try:
         env_vars = load_env_variables(required_vars=required_vars)
@@ -143,16 +202,18 @@ def main():
                 print(f"FaceNet detected {nfaces} faces.")
                 print(f"Azure detected {azure_faces} faces.")
 
-                should_continue = interact_with_user(bot, voice, synthesizer, translate, nfaces)
+                should_continue = interact_with_user(bot, voice, synthesizer, translate, nfaces, cap)
                 if not should_continue:
                     break
 
     except Exception as e:
         print(f"Fatal Error: {e}")
+
     finally:
         if 'cap' in locals() and cap.isOpened():
             cap.release()
         cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
--- large_language_model/bot_gpt.py
+++ large_language_model/bot_gpt.py
@@ -1,11 +1,13 @@
 import requests
 import base64
 from mimetypes import guess_type
+from open import AzureOpenAI
 
 
 class BotAgent:
-    def __init__(self, key, ep, model) -> None:
+    def __init__(self, key, ep, model, vision_model) -> None:
         self.key = key
+        self.vision_model=vision_model
         self.ep = ep
         self.model = model
         self.messages = {}
@@ -37,8 +39,8 @@ class BotAgent:
             # Handle request exceptions (e.g., network issues)
             print(f"Request exception: {e}")
             return None
-    
-    def local_image_to_data_url(image_path):
+
+    def local_image_to_data_url(self, image_path):
         # Guess the MIME type of the image based on the file extension
         mime_type, _ = guess_type(image_path)
         if mime_type is None:
@@ -50,34 +52,39 @@ class BotAgent:
 
         # Construct the data URL
         return f"data:{mime_type};base64,{base64_encoded_data}"
-        
-    def food_analyzer(self, prompt,image_jpg,image_path):
-        headers= {
-            "Content-Type": "application/json",
-            "api-key": self.key
-        }
-        
-        url= f"{self.ep}/openai/deployments/{self.model}/chat/completions?api-version=2024-02-01"
 
-        base64_image= base64.b64encode(image_jpg).decode("utf-8")
+    def chat_with_vision(self, image_path):
+        client = AzureOpenAI(
+            api_key=self.key,
+            api_version='2023-12-01-preview',
+            base_url=f"{self.ep}/openai/deployments/{self.vision_model}"
+        )
 
-        user_input= {"role": "user", "content": [{"type":"text","text": prompt},
-                                                 {"type":"image_url","image_url":{"url":self.local_image_to_data_url(image_path)}}]}
-
-        print(user_input)
-        payload = {"messages": [self.messages, user_input]}
-
-        try:
-            response = requests.post(url, headers=headers, json=payload)
-            response.raise_for_status()
-            response_json = response.json()
-
-            if "choices" in response_json and len(response_json["choices"]) > 0:
-                return response_json["choices"][0]["message"]["content"]
-            
-            else:
-                return None
-        
-        except requests.RequestException as e:
-            print(f"Request exception: {e}")
-            return None
+        response = client.chat.completions.create(
+            model=self.vision_model,
+            messages=[
+                self.messages,
+                {"role": "user", "content": [
+                    {
+                        "type": "text",
+                        "text": (
+                            "Analyze the following image and describe the food item depicted. Provide the following "
+                            "information:"
+                            "1. A description of what you see in the image. "
+                            "2. Whether the food item is suitable for consumption. "
+                            "3. Additional information about the food item if it is suitable. "
+                            "If the image does not show a food item, inform the user that no relevant food item was "
+                            "detected.")
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": self.local_image_to_data_url(image_path=image_path),
+                            "detail": "low"
+                        }
+                    }
+                ]}
+            ],
+            max_tokens=250,
+        )
+        return response.choices[0].message.content
